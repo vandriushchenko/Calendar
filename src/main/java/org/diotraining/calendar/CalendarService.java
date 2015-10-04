2455c8e970808a54f1783e00cd83f8a81b8ce0b9
vandriushchenko <snookyit@gmail.com>
2015-10-05 00:36:19 +0300

    Changed CalendarService to interface, added class that implements the interface

--- src/main/java/org/diotraining/calendar/CalendarService.java
+++ src/main/java/org/diotraining/calendar/CalendarService.java
@@ -1,18 +1,17 @@
 package org.diotraining.calendar;
 
+import java.util.Calendar;
 import java.util.List;
 
-public class CalendarService {
+public interface CalendarService {
 
-    public void addEvent(Event event){
+    public void addEvent(Event event);
 
-    }
+    public void createEvent(String title, List<String> emails);
 
-    public void createEvent(String title, List<String> emails){
+    public Event searchEvent(String title);
 
-    }
+    public List<Event> search(Calendar date);
 
-    public Event searchEvent(String title){
-        return new Event.Builder().build();
-    }
+    public List<Calendar[]> checkAvailability(Person ... persons);
 }
---
+++ src/main/java/org/diotraining/calendar/CalendarServiceImpl.java
@@ -0,0 +1,27 @@
+package org.diotraining.calendar;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class CalendarServiceImpl implements CalendarService{
+
+    public void addEvent(Event event) {
+
+    }
+
+    public void createEvent(String title, List<String> emails) {
+
+    }
+
+    public Event searchEvent(String title) {
+        return null;
+    }
+
+    public List<Event> search(Calendar date) {
+        return null;
+    }
+
+    public List<Calendar[]> checkAvailability(Person... persons) {
+        return null;
+    }
+}
